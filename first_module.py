2d7b5c110efb6a82d75d117242b38098b74e9d3b
211217613 <rubenavazquez@gmail.com>
2016-07-31 13:23:57 -0500

    Add test files for if __name__ == '__main__'

---
+++ first_module.py
@@ -0,0 +1,10 @@
+
+print("fist modules __name__ {}".format(__name__))
+
+def main():
+    print("HI")
+
+if __name__ == "__main__":
+    main()
+    # Is this module being run directly by python or is it being imported
+    # 
---
+++ second_module.py
@@ -0,0 +1,4 @@
+# When importing a module, the code in the module is executed
+import first_module
+
+print("Second modules __name__ {}".format(__name__))
