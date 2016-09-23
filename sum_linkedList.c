e115b2c8f80bc50ac859166ceebfeb4f8f939c05
211217613 <rubenavazquez@gmail.com>
2016-09-23 13:37:02 -0500

    ci

--- sum_linkedList.c
+++ sum_linkedList.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-
+#include <assert.h>
 
 /*
   ------------------------------         ------------------------------
@@ -17,17 +17,25 @@ typedef struct node{
 	struct node *next;
 } node_t;
 
-void print_list(node_t* list){
+int print_list(node_t* list){
+	if(NULL == list->next){
+		puts("Empty list");
+		return 1;	
+	}
 	node_t* current = list;
+
 	while(NULL != current){
 		printf("Val: %d\n", current->val);
 		current = current->next;
 	}
+	return 1;
 }
 
 void push(node_t* list, unsigned int val){
 	node_t* current = list;
 	node_t* tail = NULL;
+	// node_t* test = (node_t*)malloc(10000000000000);
+	// assert(test);
 	while(NULL != current->next){
 		current = current->next; 
 	}
@@ -42,6 +50,16 @@ void push(node_t* list, unsigned int val){
 *	int pop()
 */
 
+void populate(node_t* head){
+	node_t* current = head;
+	unsigned int index = 0;
+	for(index = 0; index < 10; index++){
+		current->val = index;
+		current->next = (node_t*)malloc(sizeof(node_t*));
+		current = current->next;
+	}
+}
+
 
 
 int main(){
@@ -49,22 +67,24 @@ int main(){
 	printf("size of node_t %lu\n", sizeof(node_t));
 	node_t *head = NULL; //pointers of type node_t, only pointer mem being used
 	node_t *tail = NULL;
+	node_t *null = NULL;
 	node_t node = {.val = 88, .next = NULL}; //Actually making memory on the stack for this
 	// malloc memory for structs
 	head = (node_t*)malloc(sizeof(node_t));
 	tail = (node_t*)malloc(sizeof(node_t));
+	null = (node_t*)malloc(sizeof(node_t));
 
-	printf("size of node_t: %lu\n",sizeof(node) );
-	printf("size of node_t*: %lu\n",sizeof(head) );
+	printf("size of node: %lu\n",sizeof(node) );
+	printf("size of head: %lu\n",sizeof(head) ); //Head is just a pointer of type node_t
+	printf("Contents of null.val: %d null.next: %p\n",null->val, null->next ); //Head is just a pointer of type node_t
 
 	if(NULL == head){
 		return 1;
 	}
-	head->val = 1;
-	(*head).val = 1;
 
-	head->next = NULL;
-	print_list(head);
-	push(head, 56);
-	print_list(head);
+	// head->next = NULL;
+	populate(head);
+	print_list(null);
+	// push(head, 56);
+	// print_list(head);
 }
